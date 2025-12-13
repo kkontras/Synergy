f721469af304889733b8e986a240ab9b24642ae8
kkontras <konstantinoskontras@gmail.com>
2025-12-13 07:42:23 +0100

    mib

--- x_mib.sh
+++ x_mib.sh
@@ -4,4 +4,4 @@ export HF_HUB_CACHE=/scratch/kkontras/data/huggingface/hub
 git pull
 conda init
 conda activate /scratch/kkontras/miniconda3/envs/synergy_new
-CUDA_VISIBLE_DEVICES=7 python train.py --config ./configs/ScienceQA/synprom_ib_gen.json  --default_config ./configs/ScienceQA/default_config_scienceqa_syn_mib.json --fold 0 --lr 0.0001 --wd 0.0001 --start_over
+CUDA_VISIBLE_DEVICES=6 python train.py --config ./configs/ScienceQA/synprom_ib_gen.json  --default_config ./configs/ScienceQA/default_config_scienceqa_syn_mib.json --fold 0 --l 1 --lr 0.0001 --wd 0.0001 --start_over

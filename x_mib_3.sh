9e2904a981bc41e5014c5ac21f05562ad46f41f3
kkontras <konstantinoskontras@gmail.com>
2026-01-20 07:00:37 +0100

    mib

--- x_mib_3.sh
+++ x_mib_3.sh
@@ -1,14 +1,14 @@
 
-CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8
-CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_image_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8
-CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_text_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8
+CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8
+CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_image_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8
+CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_text_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8
 
 for l in 1.0; do
   for lsparse in 0.1 0.5 1.0 5.0 do;
-    CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb learned --perturb_lsparse $lsparse
+    CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb learned --perturb_lsparse $lsparse
   done
-#  CUDA_VISIBLE_DEVICES=0 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb rand --perturb_pmin 0.3
-#  CUDA_VISIBLE_DEVICES=0 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb rand --perturb_pmin 0.5
-#  CUDA_VISIBLE_DEVICES=0 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb rand --perturb_pmin 0.7
+#  CUDA_VISIBLE_DEVICES=0 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb rand --perturb_pmin 0.3
+#  CUDA_VISIBLE_DEVICES=0 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb rand --perturb_pmin 0.5
+#  CUDA_VISIBLE_DEVICES=0 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --l $l --perturb rand --perturb_pmin 0.7
 done
 

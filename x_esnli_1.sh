722bd9ed9f468e2d590609670233835ac9910712
kkontras <konstantinoskontras@gmail.com>
2026-01-22 06:19:07 +0100

    mib

--- x_esnli_1.sh
+++ x_esnli_1.sh
@@ -1,5 +1,7 @@
 
 getgpu 1 80GB 60:00:00
+conda activate synergy_new
+cd /home/kkontras/orcd/scratch/Synergy
 python train.py  --config ./configs/ESNLI/cache_lora.json  --default_config ./configs/ESNLI/default_config_esnli_cache_orcd.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 5
 scancel $SLURM_JOB_ID
 

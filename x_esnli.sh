8d050a16aca5c7df2c41c956d949e04fe9def19a
kkontras <konstantinoskontras@gmail.com>
2026-01-22 05:46:18 +0100

    mib

--- x_esnli.sh
+++ x_esnli.sh
@@ -6,6 +6,11 @@ python ESNLI_CodeBook.py --split val --data_root "/home/kkontras/orcd/scratch/da
 
 
 
+python train.py  --config ./configs/ESNLI/cache_lora.json  --default_config ./configs/ESNLI/default_config_esnli_cache_orcd.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 5
+python train.py  --config ./configs/ESNLI/cache_image_lora.json  --default_config ./configs/ESNLI/default_config_esnli_cache_orcd.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 5
+python train.py  --config ./configs/ESNLI/cache_text_lora.json  --default_config ./configs/ESNLI/default_config_esnli_cache_orcd.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 5
+
+
 
 CUDA_VISIBLE_DEVICES=4 python train.py  --config ./configs/ESNLI/cache_lora_emb_clshead.json  --default_config ./configs/ESNLI/default_config_esnli_cache.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 5
 python train.py  --config ./configs/ESNLI/cache_lora.json  --default_config ./configs/ESNLI/default_config_esnli_cache_orcd.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 5

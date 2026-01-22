523606b21c88483f34ad7066cb2e0100dee48b16
kkontras <konstantinoskontras@gmail.com>
2026-01-22 06:21:50 +0100

    mib

--- x_esnli_1.sh
+++ x_esnli_1.sh
@@ -1,5 +1,5 @@
-conda activate synergy_new
-getgpu 1 80GB 60:00:00
+srun --gres=gpu:1 --time=60:00:00 -p pi_ppliang --nodelist=node2500 -c 15 --mem=65G --pty bash
+conda init
 conda activate synergy_new
 cd /home/kkontras/orcd/scratch/Synergy
 python train.py  --config ./configs/ESNLI/cache_lora.json  --default_config ./configs/ESNLI/default_config_esnli_cache_orcd.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 5

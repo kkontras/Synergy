5888a15ceb3f7983589e75f8b64b00429ca634e1
kkontras <konstantinoskontras@gmail.com>
2026-01-25 08:59:45 +0100

    mib

--- x_esnli_3.sh
+++ x_esnli_3.sh
@@ -22,5 +22,5 @@
 #CUDA_VISIBLE_DEVICES=0 python ESNLI_CodeBook.py --split validation --data_root "/scratch/kkontras/ESNLI" --flickr_images_dir "/scratch/kkontras/ESNLI/flickr30k-images/" --model_name "Qwen/Qwen3-VL-2B-Instruct" --output_dir "/scratch/kkontras/ESNLI/cache_qwen3_vl_2b_nocls_vis" --batch_size 1
 #CUDA_VISIBLE_DEVICES=2 python ESNLI_CodeBook.py --split test --data_root "/scratch/kkontras/ESNLI" --flickr_images_dir "/scratch/kkontras/ESNLI/flickr30k-images/" --model_name "Qwen/Qwen3-VL-2B-Instruct" --output_dir "/scratch/kkontras/ESNLI/cache_qwen3_vl_2b_nocls_vis" --batch_size 1
 
-CUDA_VISIBLE_DEVICES=1 python ESNLI_CodeBook.py --split train --data_root "/scratch/kkontras/ESNLI" --flickr_images_dir "/scratch/kkontras/ESNLI/flickr30k-images/" --model_name "Qwen/Qwen3-VL-2B-Instruct" --output_dir "/scratch/kkontras/ESNLI/cache_qwen3_vl_2b_nocls_vis" --batch_size 1
+CUDA_VISIBLE_DEVICES=1 python mydatasets/ESNLI/ESNLI_CodeBook.py --split train --data_root "/scratch/kkontras/ESNLI" --flickr_images_dir "/scratch/kkontras/ESNLI/flickr30k-images/" --model_name "Qwen/Qwen3-VL-2B-Instruct" --output_dir "/scratch/kkontras/ESNLI/cache_qwen3_vl_2b_nocls_vis" --batch_size 1
 CUDA_VISIBLE_DEVICES=5 python train.py  --config ./configs/ESNLI/cache_lora.json  --default_config ./configs/ESNLI/default_config_esnli_cache_mib.json --fold 0 --lr 0.0001 --wd 0.0 --batch_size 32

91944208a7cfe11b2d14eb34c4d3f1278fb213e1
kkontras <konstantinoskontras@gmail.com>
2026-01-26 23:37:32 +0100

    mib

--- x_mib_4.sh
+++ x_mib_4.sh
@@ -1,11 +1,12 @@
-CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8  --l 1 --perturb rand --perturb_pmin 0.5
 CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --start_over --l 0.1 --perturb rand --perturb_pmin 0.5
 CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --start_over --l 0.01 --perturb rand --perturb_pmin 0.5
 
 
-CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --start_over --l 1 --perturb rand --perturb_pmin 0.8
 CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --start_over --l 0.1 --perturb rand --perturb_pmin 0.8
 CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --start_over --l 0.01 --perturb rand --perturb_pmin 0.8
+
+CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8  --l 1 --perturb rand --perturb_pmin 0.5
+CUDA_VISIBLE_DEVICES=3 python train.py  --config ./configs/ScienceQA/cache_synib_lora.json  --default_config ./configs/ScienceQA/default_config_scienceqa_cache_mib.json --fold 0 --lr 0.0001 --wd 0.01 --batch_size 8 --start_over --l 1 --perturb rand --perturb_pmin 0.8
 
 
 

97b466a9c6b2e164a35d70c64a2ae07c3d9a4c43
kkontras <konstantinoskontras@gmail.com>
2026-02-07 00:02:20 +0100

    mib

--- models/Synergy_Models_Dec.py
+++ models/Synergy_Models_Dec.py
@@ -5811,7 +5811,7 @@ class QwenVL_ScienceQA_Cached_Text(nn.Module):
 
         out = self.backbone.model.language_model(
             input_ids=None,
-            position_ids = position_ids,
+            position_ids = new_pos,
             inputs_embeds=input_embeds,
             attention_mask=attention_mask,
             # visual_pos_masks=image_mask,
